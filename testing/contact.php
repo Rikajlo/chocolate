3fdc2bcf7b7af97cc3ff8473296fa65c6d63ba8e
Martin <martin.kis.98@gmail.com>
2018-11-12 23:04:21 +0100

    img kompresija

--- testing/contact.php
+++ testing/contact.php
@@ -76,7 +76,7 @@
                     </form>
                 </div>
                 <div class="col-md-6">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1383.4167075643322!2d19.661675170085086!3d46.094297841322565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474366d1b03dbbc9%3A0x4cb117142339b78e!2sVisoka+tehni%C4%8Dka+%C5%A1kola+strukovnih+studija!5e0!3m2!1sen!2srs!4v1542020891846" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1383.4167075643322!2d19.661675170085086!3d46.094297841322565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x474366d1b03dbbc9%3A0x4cb117142339b78e!2sVisoka+tehni%C4%8Dka+%C5%A1kola+strukovnih+studija!5e0!3m2!1sen!2srs!4v1542020891846" width="600" height="450" frameborder="1" style="border:0" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
