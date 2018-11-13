a6b559c8948bd0e4eea2c78c58b320d2d91a4522
trashflash <38582481+trashflash@users.noreply.github.com>
2018-11-13 01:04:16 +0100

    ads

--- testing/ads.php
+++ testing/ads.php
@@ -1,50 +1,34 @@
-<div>
+<div style="background: #6b553a">
     <div class="container">
         <div class="row">
             <div class="col-sm-12 col-lg-12">
-            CHECK OUT OUR FRIENDS!
+                <h2>CHECK OUT OUR FRIENDS WHO ALSO HAVE THEIR OWN WEBSITES!</h2>
             </div>
         </div>
         <div class="row">
-            <div class="col-sm-3 col-lg-3">
+            <div class="col-sm-6 col-lg-3 col-12">
                 <div class="widget">
-                    <h5 class="widgetheading">Our Contact</h5>
-                    <address>
-                        <strong>This website is made by students for a school project</strong><br>
-                        JC Main Road, Near Silnile tower<br>
-                        Pin-21542 NewYork US.</address>
-                    <p>
-                        <i class="icon-phone"></i> (123) 456-789 - 1255-12584 <br>
-                        <i class="icon-envelope-alt"></i> email@domainname.com
-                    </p>
+                    <h5 class="widgetheading">Healthy Beef</h5>
+                    <a href="http://healthy.beef.live"><img src="img/lekvar/ad1.JPG" alt="Smiley face" height="250" width="250"></a>
                 </div>
             </div>
-            <div class="col-sm-3 col-lg-3">
+            <div class="col-sm-6 col-lg-3 col-12">
                 <div class="widget">
-                    <h5 class="widgetheading">Quick Links</h5>
-                    <ul class="link-list">
-                        <li><a class="waves-effect waves-dark" href="#">Latest Events</a></li>
-                        <li><a class="waves-effect waves-dark" href="#">Terms and conditions</a></li>
-                        <li><a class="waves-effect waves-dark" href="#">Privacy policy</a></li>
-                        <li><a class="waves-effect waves-dark" href="#">Career</a></li>
-                        <li><a class="waves-effect waves-dark" href="#">Contact us</a></li>
+                    <h5 class="widgetheading">WOMEN'S SHOES</h5>
+                    <a href="https://radomirowicmarija97.000webhostapp.com/"><img src="img/lekvar/ad3.JPG" alt="site ad photo" height="250" width="250"></a>
                     </ul>
                 </div>
             </div>
-            <div class="col-sm-3 col-lg-3">
+            <div class="col-sm-6 col-lg-3 col-12">
                 <div class="widget">
-                    <h5 class="widgetheading">Latest posts</h5>
-                    <ul class="link-list">
-                        <li><a class="waves-effect waves-dark" href="#">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</a></li>
-                        <li><a class="waves-effect waves-dark" href="#">Pellentesque et pulvinar enim. Quisque at tempor ligula</a></li>
-                        <li><a class="waves-effect waves-dark" href="#">Natus error sit voluptatem accusantium doloremque</a></li>
-                    </ul>
+                    <h5 class="widgetheading">PET STUFF</h5>
+                    <a href="http://petstuff.000webhostapp.com/"><img src="img/lekvar/ad2.JPG" alt="site ad photo" height="250" width="250"></a>
                 </div>
             </div>
-            <div class="col-sm-3 col-lg-3">
+            <div class="col-sm-6 col-lg-3 col-12">
                 <div class="widget">
-                    <h5 class="widgetheading">Recent News</h5>
-                    <a href=""><img src="ad.png" alt="Smiley face" height="350" width="350"></a>
+                    <h5 class="widgetheading">RIBOLOV NA DLANU</h5>
+                    <a href="https://ribolov-na-dlanu.000webhostapp.com/index.php?"><img src="img/lekvar/ad4.JPG" alt="site ad photo" height="250" width="250"></a>
                 </div>
             </div>
         </div>
